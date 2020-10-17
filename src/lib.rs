b045fa07a9e7e0eba3487aeef11e5a40892d2192
Jens Reimann <jreimann@redhat.com>
2020-10-18 00:03:44 +0200

    Fixup example

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,9 @@
 #![recursion_limit = "256"]
-mod button;
-mod card;
-mod content;
-mod form;
-mod gallery;
-mod logo;
-mod page;
-mod pagesection;
-mod pagesidebar;
 
-use button::*;
-use card::*;
-use content::*;
-use form::*;
-use gallery::*;
-use logo::*;
-use page::*;
-use pagesection::*;
-use pagesidebar::*;
+use patternfly_yew::*;
 
-use std::cell::RefCell;
-use std::ops::Deref;
-use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum Variant {
-    Primary,
-    Secondary,
-    Link,
-}
-
-impl Variant {
-    pub fn as_class(&self) -> &str {
-        match self {
-            Variant::Primary => "pf-m-primary",
-            Variant::Secondary => "pf-m-secondary",
-            Variant::Link => "pf-m-link",
-        }
-    }
-}
-
-impl Default for Variant {
-    fn default() -> Self {
-        Variant::Primary
-    }
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum Icon {
-    PlusCircleIcon,
-}
-
-impl Icon {
-    pub fn as_class(&self) -> &str {
-        match self {
-            Icon::PlusCircleIcon => "fas fa-plus-circle",
-        }
-    }
-}
-
-pub struct WeakComponentLink<COMP: Component>(Rc<RefCell<Option<ComponentLink<COMP>>>>);
-
-impl<COMP: Component> Clone for WeakComponentLink<COMP> {
-    fn clone(&self) -> Self {
-        Self(Rc::clone(&self.0))
-    }
-}
-
-impl<COMP: Component> Default for WeakComponentLink<COMP> {
-    fn default() -> Self {
-        Self(Rc::default())
-    }
-}
-
-impl<COMP: Component> Deref for WeakComponentLink<COMP> {
-    type Target = Rc<RefCell<Option<ComponentLink<COMP>>>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<COMP: Component> PartialEq for WeakComponentLink<COMP> {
-    fn eq(&self, other: &Self) -> bool {
-        Rc::ptr_eq(&self.0, &other.0)
-    }
-}
 
 struct Model {
     link: ComponentLink<Self>,
@@ -137,7 +53,7 @@ impl Component for Model {
                 }}>
                 <PageSection variant=PageSectionVariant::Light limit_width=true>
                     <Content>
-                        <h1>{"Bookings"}</h1>
+                        <h1>{"Counting clicks"}</h1>
                     </Content>
                 </PageSection>
                 <PageSection>
@@ -146,11 +62,11 @@ impl Component for Model {
                             selectable=true
                             selected=true
                             title={html_nested!{<>
-                                {"Title"}
+                                {"Clicks"}
                             </>}}
                             >
 
-                            <div>{"Bar"}</div>
+                            <p>{ self.value }</p>
 
                         </Card>
                     </Gallery>
@@ -158,7 +74,6 @@ impl Component for Model {
                 <PageSection>
                     <Form>
                         <Button label="Add One" icon=Some(Icon::PlusCircleIcon) variant=Variant::Link onclick=self.link.callback(|_| Msg::AddOne)/>
-                        <p>{ self.value }</p>
                     </Form>
                 </PageSection>
             </Page>
