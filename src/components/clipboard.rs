466e099384e6e326d7f78a3492ca886b07921d9f
Jens Reimann <jreimann@redhat.com>
2021-02-08 16:46:05 +0100

    Add some more clipboard examples

--- src/components/clipboard.rs
+++ src/components/clipboard.rs
@@ -23,11 +23,17 @@ impl Component for ClipboardExample {
 
     fn view(&self) -> Html {
         let example1 = example2! ("Clipboard" => "clipboard.1.example");
+        let example2 = example2! ("Clipboard (readonly)" => "clipboard.2.example");
+        let example3 = example2! ("Clipboard (code, expandable)" => "clipboard.3.example");
+        let example4 = example2! ("Clipboard (readonly, expanded)" => "clipboard.4.example");
 
         html! {
             <>
                 <ExamplePage title="Clipboard">
                     {example1}
+                    {example2}
+                    {example3}
+                    {example4}
                 </ExamplePage>
             </>
         }
