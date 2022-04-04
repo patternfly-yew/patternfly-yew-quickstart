7d3cf32c5f6d7da7a6bc1913a1c29a3ff0aedb00
Jens Reimann <jreimann@redhat.com>
2022-04-04 16:02:22 +0200

    feat: add example with negative values

--- src/components/slider.rs
+++ src/components/slider.rs
@@ -7,12 +7,14 @@ use yew::prelude::*;
 pub fn slider() -> Html {
     let example1 = example2! ("Slider" => "slider.1.example");
     let example2 = example2! ("Slider (Labels)" => "slider.2.example");
+    let example3 = example2! ("Slider (negative)" => "slider.3.example");
 
     html! (
         <>
             <ExamplePage title="Sliders">
                 {example1}
                 {example2}
+                {example3}
             </ExamplePage>
         </>
     )
