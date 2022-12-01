181fdae7ebc40961b2156345348f6c61b7420219
Jens Reimann <jreimann@redhat.com>
2022-12-01 15:46:39 +0100

    refactor: add example for selector inside of form

--- src/components/context_selector.rs
+++ src/components/context_selector.rs
@@ -3,26 +3,18 @@ use crate::{example::ExamplePage, example2};
 use patternfly_yew::*;
 use yew::prelude::*;
 
-pub struct ContextSelectorExample {}
+#[function_component(ContextSelectorExample)]
+pub fn example() -> Html {
+    let examples: Vec<Html> = vec![
+        example2! ("Context Selector" => "context_selector.1.example"),
+        example2! ("Context Selector (form)" => "context_selector.2.example"),
+    ];
 
-impl Component for ContextSelectorExample {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn view(&self, _: &Context<Self>) -> Html {
-        let examples: Vec<Html> =
-            vec![example2! ("Context Selector" => "context_selector.1.example")];
-
-        html! {
-            <>
-                <ExamplePage title="Context Selector">
-                    { for examples }
-                </ExamplePage>
-            </>
-        }
+    html! {
+        <>
+            <ExamplePage title="Context Selector">
+                { for examples }
+            </ExamplePage>
+        </>
     }
 }
