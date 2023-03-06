4103e6319915696ed37f0964f7d949234095bfd4
Jens Reimann <jreimann@redhat.com>
2023-03-06 15:58:35 +0100

    feat(slider): add ticks and snap mode example

--- src/components/slider/mod.rs
+++ src/components/slider/mod.rs
@@ -8,6 +8,7 @@ pub fn slider() -> Html {
     let example1 = example! ("Slider" => "slider.1.example");
     let example2 = example! ("Slider (Labels)" => "slider.2.example");
     let example3 = example! ("Slider (negative)" => "slider.3.example");
+    let example4 = example! ("Slider (ticks)" => "slider.4.example");
 
     html! (
         <>
@@ -15,6 +16,7 @@ pub fn slider() -> Html {
                 {example1}
                 {example2}
                 {example3}
+                {example4}
             </ExamplePage>
         </>
     )
