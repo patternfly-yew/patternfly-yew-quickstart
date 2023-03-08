22a46b9ee0090920d440e3d6e394a3bc0e76b0e8
Jens Reimann <jreimann@redhat.com>
2023-03-08 14:15:26 +0100

    feat: add and restructure examples

--- src/components/form/mod.rs
+++ src/components/form/mod.rs
@@ -3,100 +3,17 @@ use crate::{example, example::ExamplePage};
 use patternfly_yew::*;
 use yew::prelude::*;
 
-pub struct FormExample {}
+#[function_component(FormExample)]
+pub fn view() -> Html {
+    let example1 = example! {"Basic Form" => "form.1.example" };
+    let example2 = example! {"Form States" => "form.2.example" };
+    let example3 = example! {"Validation" => "form.3.example" };
 
-impl Component for FormExample {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn view(&self, _: &Context<Self>) -> Html {
-        let example = example! {"Text Input" => "form.1.example" };
-
-        html! {
-            <>
-                <ExamplePage title="Form">
-                    <Form>
-                        <FormGroup label="Test">
-                            <Button label="Click me" variant={ButtonVariant::Primary}/>
-                        </FormGroup>
-                        <FormGroup label="Test" required=true helper_text={Some("Some help for you.".into())}>
-                            <Button label="Click me too" variant={ButtonVariant::Secondary}/>
-                        </FormGroup>
-                    </Form>
-
-                    {
-                        example
-                    }
-
-                    <Title level={Level::H2}>{"Form Validation"}</Title>
-
-                    { Self::form_validation() }
-
-                    <Title level={Level::H2}>{"More"}</Title>
-
-                    <Form>
-                        <FormGroup label="Normal">
-                            <TextInput/>
-                        </FormGroup>
-                        <FormGroup label="Success">
-                            <TextInput state={InputState::Success}/>
-                        </FormGroup>
-                        <FormGroup label="Warning">
-                            <TextInput state={InputState::Warning}/>
-                        </FormGroup>
-                        <FormGroup label="Error">
-                            <TextInput state={InputState::Error}/>
-                        </FormGroup>
-                        <FormGroup label="Read Only">
-                            <TextInput readonly=true/>
-                        </FormGroup>
-                        <FormGroup label="Disabled">
-                            <TextInput disabled=true/>
-                        </FormGroup>
-                        <FormGroup label="Search">
-                            <TextInput icon={TextInputIcon::Search}/>
-                        </FormGroup>
-                        <FormGroup label="Calendar">
-                            <TextInput icon={TextInputIcon::Calendar}/>
-                        </FormGroup>
-                        <FormGroup label="Clock">
-                            <TextInput icon={TextInputIcon::Clock}/>
-                        </FormGroup>
-                    </Form>
-                </ExamplePage>
-            </>
-        }
-    }
-}
-
-impl FormExample {
-    pub fn form_validation() -> Html {
-        html!(
-            <Form>
-                <FormGroupValidated<TextInput>
-                    label="Some text"
-                    required=true
-                    validator={Validator::from(|ctx: ValidationContext<String>| {
-                        if ctx.value.is_empty() {
-                            ValidationResult::error("Must not be empty")
-                        } else if ctx.value.contains("foo") {
-                            ValidationResult::warning("Should not contain 'foo'")
-                        } else if ctx.value == "Nur ein Wort" {
-                            ValidationResult::new(InputState::Success, "Congratulations, you found the magic value!")
-                        } else {
-                            ValidationResult::default()
-                        }
-                    })}
-                >
-                    <TextInput
-                        placeholder="Enter some text"
-                    />
-                </FormGroupValidated<TextInput>>
-            </Form>
-        )
-    }
+    html! (
+        <ExamplePage title="Form">
+            { example1 }
+            { example2 }
+            { example3 }
+        </ExamplePage>
+    )
 }
