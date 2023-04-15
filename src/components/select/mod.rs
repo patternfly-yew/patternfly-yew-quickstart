3ee9cc3dd1d26783ebfdd143922edec5f414227e
TuxCoder <git@o-g.at>
2023-04-15 10:03:48 +0200

    add callback example to `select` component

--- src/components/select/mod.rs
+++ src/components/select/mod.rs
@@ -5,11 +5,14 @@ use yew::prelude::*;
 
 #[function_component(SelectExample)]
 pub fn select_example() -> Html {
+    let example6_state = use_state(|| "".to_string());
+
     let example1 = example! ("Select" => "select.1.example");
     let example2 = example! ("Select (with divider)" => "select.2.example");
     let example3 = example! ("Select (with groups)" => "select.3.example");
     let example4 = example! ("Select Checkbox (with groups)" => "select.4.example");
     let example5 = example! ("Select (multiple)" => "select.5.example");
+    let example6 = example! ("Select (multiple) with callback" => "select.6.example");
 
     html! {
         <>
@@ -19,6 +22,7 @@ pub fn select_example() -> Html {
                 {example3}
                 {example4}
                 {example5}
+                {example6}
             </ExamplePage>
         </>
     }
