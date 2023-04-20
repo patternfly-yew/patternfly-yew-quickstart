9efc3cf5ca30bf9817ab0a70b71e7532651f7688
Jens Reimann <jreimann@redhat.com>
2023-04-20 15:06:42 +0200

    feat: set the log level depending on the profile

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,15 @@ mod icons;
 mod index;
 mod layouts;
 
-use log::Level;
 use wasm_bindgen::prelude::*;
 
+#[cfg(not(debug_assertions))]
+const LOG_LEVEL: log::Level = log::Level::Info;
+#[cfg(debug_assertions)]
+const LOG_LEVEL: log::Level = log::Level::Trace;
+
 pub fn main() -> Result<(), JsValue> {
-    wasm_logger::init(wasm_logger::Config::new(Level::Debug));
+    wasm_logger::init(wasm_logger::Config::new(LOG_LEVEL));
     yew::Renderer::<app::Application>::new().render();
     Ok(())
 }
