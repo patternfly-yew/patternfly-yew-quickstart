8d1145ce4b79be4cd07f97fbe04ae874ae977938
Jens Reimann <jreimann@redhat.com>
2023-04-27 15:07:46 +0200

    docs: show a note about showing a modal

--- src/components/modal/mod.rs
+++ src/components/modal/mod.rs
@@ -8,9 +8,18 @@ pub fn modal_example() -> Html {
     let example1 = example!("Basic Medium Modal" => "modal.1.example");
     let example2 = example!("Modal with a form" => "modal.2.example");
 
+    let subtitle = Html::from_html_unchecked(
+        r#"
+<p>
+    The actual modal content. See <code>Backdrop</code> for showing a modal.
+</p>
+"#
+        .into(),
+    );
+
     html! {
         <>
-            <ExamplePage title="Modal">
+            <ExamplePage title="Modal" {subtitle}>
                 {example1}
                 {example2}
             </ExamplePage>
