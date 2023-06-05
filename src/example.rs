9f3e42ca07db0f6426008fa4c0e3dd161359ed41
Philip Ridout <phill.ridout@gmail.com>
2023-06-05 16:46:44 +0100

    Upgrade to PF5

--- src/example.rs
+++ src/example.rs
@@ -113,10 +113,10 @@ pub fn example(props: &ExampleProps) -> Html {
                 <FlexItem modifiers={[FlexModifier::Flex1]}>
                     <Title level={Level::H3} size={Size::Large}>{"Code"}</Title>
 
-                    <div class="pf-c-code-editor">
-                        <div class="pf-c-code-editor__main">
-                            <div class="pf-c-code-editor__code">
-                                <pre class="pf-c-code-editor__code-pre">
+                    <div class="pf-v5-c-code-editor">
+                        <div class="pf-v5-c-code-editor__main">
+                            <div class="pf-v5-c-code-editor__code">
+                                <pre class="pf-v5-c-code-editor__code-pre">
                                     {&props.code}
                                 </pre>
                             </div>
