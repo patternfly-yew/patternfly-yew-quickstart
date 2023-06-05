632493fa6d7e1b3d6d81336a1a3dd3586c721f9b
Philip Ridout <phill.ridout@gmail.com>
2023-06-05 21:03:40 +0100

    Badge examples

--- src/components/badge/mod.rs
+++ src/components/badge/mod.rs
@@ -5,8 +5,8 @@ use yew::prelude::*;
 
 #[function_component(BadgeExample)]
 pub fn badge_example() -> Html {
-    let example1 = example! ("Badge" => "badge.1.example");
-    let example2 = example! ("Badge (Read-only)" => "badge.2.example");
+    let example1 = example! ("Read" => "badge.1.example");
+    let example2 = example! ("Unread with Screen Reader Text" => "badge.2.example");
 
     html! {
         <>
