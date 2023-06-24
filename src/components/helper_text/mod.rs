f4edfdf3c4b0b2eabed8dc088ca0fd65a209f6b0
Philip Ridout <phill.ridout@gmail.com>
2023-06-24 22:37:16 +0100

    rework helpertext examples for modified component

--- src/components/helper_text/mod.rs
+++ src/components/helper_text/mod.rs
@@ -6,13 +6,15 @@ use yew::prelude::*;
 #[function_component(HelperTextExample)]
 pub fn helper_text() -> Html {
     let example1 = example! ("HelperText" => "helper_text.1.example");
-    let example2 = example! ("HelperText (With custom icon)" => "helper_text.2.example");
+    let example2 = example! ("HelperText (using an ul component)" => "helper_text.2.example");
+    let example3 = example! ("HelperText (icon property options)" => "helper_text.3.example");
 
     html! {
         <>
-            <ExamplePage title="DescriptionList">
+            <ExamplePage title="HelperText">
                 {example1}
                 {example2}
+                {example3}
             </ExamplePage>
         </>
     }
