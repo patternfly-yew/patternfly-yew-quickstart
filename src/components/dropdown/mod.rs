216c0ef3fbccfd737b4d4b7418a08781cabd47de
Jens Reimann <ctron@dentrassi.de>
2023-07-24 10:08:26 +0200

    refactor: some cleanups, new APIs and updated deps

--- src/components/dropdown/mod.rs
+++ src/components/dropdown/mod.rs
@@ -2,45 +2,22 @@ use crate::{example, example::ExamplePage};
 use patternfly_yew::prelude::*;
 use yew::prelude::*;
 
-pub enum Msg {
-    ShowToast(Toast),
-}
+#[function_component(DropdownExample)]
+pub fn dropdown_example() -> Html {
+    let toaster = use_toaster().unwrap();
+    let show_toast = use_callback(|text, toaster| toaster.toast(text), toaster);
 
-pub struct DropdownExample {}
+    let example1 = example! ("Dropdown" => "dropdown.1.example");
+    let example2 = example! ("Dropdown (Kebab)" => "dropdown.2.example");
+    let example3 = example! ("Dropdown (User)" => "dropdown.3.example");
 
-impl Component for DropdownExample {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Self::Message::ShowToast(toast) => {
-                if let Some((toaster, _)) = ctx.link().context::<Toaster>(Callback::default()) {
-                    toaster.toast(toast);
-                }
-
-                false
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let example1 = example! ("Dropdown" => "dropdown.1.example");
-        let example2 = example! ("Dropdown (Kebab)" => "dropdown.2.example");
-        let example3 = example! ("Dropdown (User)" => "dropdown.3.example");
-
-        html! {
-            <>
-                <ExamplePage title="Dropdown">
-                    {example1}
-                    {example2}
-                    {example3}
-                </ExamplePage>
-            </>
-        }
+    html! {
+        <>
+            <ExamplePage title="Dropdown">
+                {example1}
+                {example2}
+                {example3}
+            </ExamplePage>
+        </>
     }
 }
--- src/components/menu/mod.rs
+++ src/components/menu/mod.rs
@@ -4,6 +4,9 @@ use yew::prelude::*;
 
 #[function_component(MenuExample)]
 pub fn menu_example() -> Html {
+    let toaster = use_toaster().unwrap();
+    let action = use_callback(|_, toaster| toaster.toast("Clicked it!"), toaster);
+
     let example1 = example!("Menu Toggle" => "menu.1.example");
     let example2 = example!("Menu" => "menu.2.example");
     let example3 = example!("Full Menu" => "menu.3.example");
