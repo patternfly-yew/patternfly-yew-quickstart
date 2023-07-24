8523b7c5182c67fde67639e037c318d73a3c2d23
Jens Reimann <ctron@dentrassi.de>
2023-07-24 12:57:22 +0200

    refactor: convert into function component

--- src/components/tooltip/mod.rs
+++ src/components/tooltip/mod.rs
@@ -2,27 +2,17 @@ use crate::{example, example::ExamplePage};
 use patternfly_yew::prelude::*;
 use yew::prelude::*;
 
-pub struct TooltipExample {}
+#[function_component(TooltipExample)]
+pub fn tooltip() -> Html {
+    let example1 = example! ("Tooltip Popup" => "tooltip.1.example");
+    let example2 = example! ("Tooltip" => "tooltip.2.example");
 
-impl Component for TooltipExample {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn view(&self, _: &Context<Self>) -> Html {
-        let example1 = example! ("Tooltip Popup" => "tooltip.1.example");
-        let example2 = example! ("Tooltip" => "tooltip.2.example");
-
-        html! {
-            <>
-                <ExamplePage title="Tooltip">
-                    {example1}
-                    {example2}
-                </ExamplePage>
-            </>
-        }
+    html! {
+        <>
+            <ExamplePage title="Tooltip">
+                {example1}
+                {example2}
+            </ExamplePage>
+        </>
     }
 }
