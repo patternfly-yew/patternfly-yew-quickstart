2362928938b57f1d495b7c0f10a686a363631a72
Jens Reimann <ctron@dentrassi.de>
2023-08-30 18:15:53 +0200

    fix: consider the base url when building links

--- src/full/mod.rs
+++ src/full/mod.rs
@@ -4,6 +4,7 @@ pub use self::login_page::*;
 
 use crate::example::*;
 use patternfly_yew::prelude::*;
+use url::Url;
 use yew::prelude::*;
 
 #[derive(Clone, PartialEq, Properties)]
@@ -11,11 +12,21 @@ pub struct Props {
     pub url: String,
 }
 
+fn build_url(url: &str) -> Option<String> {
+    let base = gloo_utils::document()
+        .base_uri()
+        .ok()
+        .flatten()
+        .and_then(|url| Url::parse(&url).ok())?;
+
+    base.join(url).ok().map(|url| url.to_string())
+}
+
 #[function_component(FullPageExample)]
 pub fn full_page_example(props: &Props) -> Html {
     let url = props.url.clone();
-
     let onclick = move |_| {
+        let url = build_url(&url).unwrap_or_else(|| url.clone());
         gloo_utils::window()
             .open_with_url_and_target(&url, "patternfly-yew-example")
             .ok();
