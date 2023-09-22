e6ef8f522622bdd4f18a28589af0c536a70a954c
rick <rick@gnous.eu>
2023-09-22 12:26:21 +0200

    feat(date): add examples for calendar and datepicker

---
+++ src/components/date/calendar/mod.rs
@@ -0,0 +1,25 @@
+use crate::{example, example::ExamplePage};
+use patternfly_yew::prelude::*;
+use yew::prelude::*;
+use chrono::*;
+
+#[function_component(CalendarExample)]
+pub fn datepicker_example() -> yew::Html {
+    let date = use_state(|| NaiveDate::from_ymd_opt(2011, 1, 14).unwrap());
+    let onchange = {
+        let date = date.clone();
+        Callback::from(move |d| date.set(d))
+    };
+
+    let select_date = example! ("Selectable date" => "calendar.1.example");
+    let range = example! ("Date range" => "calendar.2.example");
+
+    html! {
+        <>
+            <ExamplePage title="Calendar month">
+                { select_date }
+                { range }
+            </ExamplePage>
+        </>
+    }
+}
