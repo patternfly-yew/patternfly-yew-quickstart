60b127bf2dc77073f8be3f3b5c40ed34f2ed9330
Ari <ari@alpico.io>
2023-11-21 13:21:02 +0100

    feat: implement search input (#37)
    * feat: implement search input
    
    * chore: bump pf-yew version

---
+++ src/components/search_input/mod.rs
@@ -0,0 +1,25 @@
+use crate::{example, example::ExamplePage};
+
+use patternfly_yew::prelude::*;
+use yew::prelude::*;
+
+#[function_component(SearchInputExample)]
+pub fn search_input() -> Html {
+    let example1 = example! ("Basic" => "search_input.1.example");
+    let example2 = example! ("Match with result count" => "search_input.2.example");
+    let example3 = example! ("Match with navigable options" => "search_input.3.example");
+    let example4 = example! ("With submit button" => "search_input.4.example");
+    let example5 = example! ("With expandable button" => "search_input.5.example");
+
+    html! {
+        <>
+            <ExamplePage title="Search Input">
+                {example1}
+                {example2}
+                {example3}
+                {example4}
+                {example5}
+            </ExamplePage>
+        </>
+    }
+}
